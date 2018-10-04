efa4d18968c605e2e995d813f5273973b0b25081
Raj-Madheshia <rajsmadheshia@gmail.com>
2018-10-04 23:19:51 +0530

    creation of table of each quiz question

---
+++ pages/teacher/createquiz.php
@@ -0,0 +1,92 @@
+
+<!Doctype html>
+<html>
+<head>
+<title>Add Questions</title>
+<meta charset="UTF-8"/>
+
+<style type="text/css">
+aside {
+	background-color: #123456;
+	width:20%;
+	height:500px;
+	float:left;
+	margin:20px;
+	margin-top: 60px;
+}
+section {
+	background-color: #eeeee9;
+	width:70%;
+	height: 625px;
+	float:left;
+	margin:20px auto;
+}
+#questions {
+	width:90%;
+	height:200px;
+    border:solid 3px #123456;
+	 
+}
+#que {
+	margin-top: 30px;
+	margin-left: 40px;
+	margin-bottom: 10px;
+	font-size: 16px;
+
+}
+#choice{
+	padding:20px;
+
+}
+button{
+	font-size: 16px;
+	padding:5px;
+}
+#finishbtn{
+	float:right;
+	margin: 20px;
+}
+#resetbtn{
+	margin-left: 25px;
+}
+#addqbtn{
+	margin-left:44px;
+}
+input[type="text"],{
+	width: 20px;
+}
+</style>
+
+</head>
+	<body>
+			<aside >
+			<h1>Question Numbers</h3>
+				
+			</aside>
+			<section>
+				<form action="" method="post">
+				<div id="que">
+					<h2>Enter Question:- </h2>
+					<div >
+						<textarea  name="Questions" id="questions"></textarea>
+					</div>
+					<div id="choice">
+						<input type="radio" name="option" value="o1">
+						<input type="text" name="ot1" style="width: 30%; padding:8px; border:solid 2px #123456;"><br><br>
+						<input type="radio" name="option" value="o1">
+						<input type="text" name="ot2" style="width: 30%; padding:8px; border:solid 2px #123456;"><br><br>
+						<input type="radio" name="option" value="o1">
+						<input type="text" name="ot3" style="width: 30%; padding:8px; border:solid 2px #123456;"><br><br>
+						<input type="radio" name="option" value="o1">
+						<input type="text" name="ot4" style="width: 30%; padding:8px; border:solid 2px #123456;"><br><br>
+					</div>
+					<div>
+						<button id="addqbtn" style="background-color:#123456;color:white">Add Question</button>
+						<button id="resetbtn" style="background-color:#123456;color:white">Reset</button>
+						<button id="finishbtn" style="background-color:#123456;color:white">Finish</button>
+					</div>
+				</div>
+			</form>
+			</section>
+	</body>
+</html
