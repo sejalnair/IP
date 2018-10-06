378a7a3accb566fe476a0a307ac62d890ba637c8
Raj-Madheshia <rajsmadheshia@gmail.com>
2018-10-06 08:58:36 +0530

    small changes

--- includes/addquestions.php
+++ includes/addquestions.php
@@ -14,4 +14,17 @@
         mysqli_query($conn,$sql);
         header('Location: ../pages/teacher/createquiz.php');
     }
+    if(isset($_POST['reset'])){
+		header('Location: ../pages/teacher/createquiz.php');
+    }
+    if(isset($_POST['finish'])){
+        setcookie('class',null, -1, '/');
+		setcookie('subject',null, -1, '/');
+		setcookie('time',null, -1, '/');
+		setcookie('date',null, -1, '/');
+		setcookie('title',null, -1, '/');
+        setcookie('subtitle',null, -1, '/');
+        setcookie('tablename',null, -1, '/');
+        header('Location: ../pages/teacher/home.php');
+    }
 ?>
---
+++ pages/teacher/updatequiz.php
@@ -0,0 +1,3 @@
+<?php  
+    include '../../includes/update.php';
+echo $updatequestion;?>
