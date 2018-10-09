73ba8e6b851cb6f3d1509f8ea9f34fc563df082a
Pranali <sunilpranali1212@gmail.com>
2018-10-09 21:51:37 +0530

    Admin functionality

---
+++ includes/admin/addstu.php
@@ -0,0 +1,41 @@
+<?php
+    include "../../includes/dbh.inc.php";
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    $class = $_POST['class'];
+    $rollno = $_POST['rollno'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $emailid = $_POST['emailid'];
+    $password = $_POST['password'];
+    $name = $fname." ".$lname;
+
+    echo $class,$rollno,$name,$emailid,$password;
+
+    if($class != ''||$rollno != ''||$name != ''||$emailid != ''||$password !=''){    
+        
+        $sql ="INSERT INTO student_login (class,rollno,name, emailid, password) VALUES ('$class','$rollno','$name', '$emailid', '$password')";
+        
+        if(mysqli_query($conn,$sql) === TRUE) {
+            echo '<script language="javascript">';
+            echo 'confirm("New record created successfully")';
+            echo '</script>';
+            echo '<script language="javascript">';
+            echo "window.location = ('../../pages/admin/home.php');\n";
+            echo '</script>';
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        
+        
+    //header("Location: ../../pages/admin/home.php");
+
+    }
+    echo '<script language="javascript">';
+    echo 'alert("Name or EmailID or Password not entered")';
+    echo '</script>';
+    mysqli_close($conn);
+?>
---
+++ includes/admin/addt.php
@@ -0,0 +1,35 @@
+<?php
+    include "dbh.inc.php";
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    $name = $_POST['name'];
+    $emailid = $_POST['emailid'];
+    $password = $_POST['password'];
+    mysqli_free_result($t);
+    if($name != ''||$emailid != ''||$password !=''){    
+        
+        $sql ="INSERT INTO teacher_login (name, emailid, password) VALUES ('$name', '$emailid', '$password')";
+        
+        if(mysqli_query($conn,$sql) === TRUE) {
+            echo '<script language="javascript">';
+            echo 'confirm("New record created successfully")';
+            echo '</script>';
+            echo '<script language="javascript">';
+            echo "window.location = ('../../pages/admin/home.php');\n";
+            echo '</script>';
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        
+        
+    header("Location: ../../pages/admin/home.php");
+
+    }
+    echo '<script language="javascript">';
+    echo 'alert("Name or EmailID or Password not entered")';
+    echo '</script>';
+    mysqli_close($conn);
+?>
