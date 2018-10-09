e90ddae469ab9bc6e9277ef6d90ebabd7f8df8a9
sejalnair <sejalnair9@gmail.com>
2018-10-09 21:30:30 +0530

    edited

--- includes/crtquiz.php
+++ includes/crtquiz.php
@@ -1,21 +1,29 @@
 <?php
-	include "dbh.inc.php";
-	if($conn){
-		if(isset($_POST['startqz'])){
-			$email = mysqli_real_escape_string($conn,$_POST['Title']);
-			$password = mysqli_real_escape_string($conn,$_POST['Subtitle']);
-			$sql = "select Title,Subtitle from d15;";
-			$result = mysqli_query($conn,$sql);
+ /*if(isset($_POST['createqz'])){
+	setcookie('class',$_POST['class'],time() +86400, '/');
+	setcookie('subject',$_POST['subject'],time() +86400, '/');
+	setcookie('time',$_POST['exam_time'],time() +86400, '/');
+	setcookie('date',$_POST['exam_date'],time() +86400, '/');
+	setcookie('title',$_POST['title'],time() +86400, '/');
+	setcookie('subtitle',$_POST['subtitle'],time() +86400, '/');
 
-			if(mysqli_num_rows($result) === 1){
-				while($row = mysqli_fetch_assoc($result)){
-					echo "Title: " . $row["Title"]. " - Subtitle: " . $row["Subtitle"]. "<br>";
-          		 	$_SESSION['Title'] = $row['Title'];
-                    $_SESSION['Subtitle'] = $row['Subtitle'];
-					
-					//header("Location: ../student/home.php");
-					}
-				}	
-			}
-	}
+	$class = $_POST['class'] ;
+	$subject = $_POST['subject'];
+	$date = $_POST['exam_date'];
+	$time = $_POST['exam_time'] ;
+	$title = $_POST['title'];
+	$subtitle = $_POST['subtitle'];*/
+
+$sql="SELECT Title,Subtitle FROM d20 where id=1";
+if ($result=mysqli_query($conn,$sql)) {
+  // Fetch one and one row
+	  while ($row=mysqli_fetch_row($result))	{
+		echo"<center><font size='5'><b>QUIZ TITLE: </b></font><label>".$row[0]."</label></center><br>";
+		echo"<center><font size='5'><b>QUIZ SUBTITLE: </b></font><label>".$row[1]."</label></center>";
+	
+}
+  // Free result set
+  mysqli_free_result($result);
+}
+ 
 ?>
--- pages/student/home.php
+++ pages/student/home.php
@@ -1,6 +1,7 @@
 <?php
 	include '../../includes/studentlogin.php';
-	include '../../includes/crtquiz.php';
+	
+	include "../../includes/dbh.inc.php";
 
 ?>
 
@@ -16,9 +17,15 @@
 			background-color: #123456;
 			float: right;
 			width: 10%;
-			padding: 5px;
+			padding: 15px;
 			margin-right: 20px;
+			border:unset;
+			border-radius:20px;
+			
 
+		}
+		.subbtn{
+			/* display:none; */
 		}
 		.heading{
 			font-family: "Times New Roman", Times, serif;
@@ -49,34 +56,33 @@
 <body>
 		<form  action="../../includes/crtquiz.php" method="post">
 		<header>
-			<div style="width:100%;height:100px;background-color: #eeee">
+			<div style="width:100%;height:100px;background-color: #eeee;font-size:30px;">
 				<label>Name:</label>
-				<input type="text" name="name" value="<?php echo $_SESSION['Name'];?>">
-				<label>Class:</label>
-				<input type="text"name="class" value="<?php echo $_SESSION['Class'];?>">
+				<label><?php echo $_SESSION['Name'];?></label>
+				<label style="margin-left:50px;">Class:</label>
+				<label><?php echo $_SESSION['Class'];?></label>
 				<input type="button" name="pswd" value="Change_password" class="btn1" style="size: 10px;color:white" />
 				<input type="button" name="logout" value="Logout" class="btn1" style="size: 10px;color:white" />
 
 			</div>
 		</header>
-		<div style="width:15%;margin-top:70px;float:left;">
-			  <input type="button" name="Sub1" value="Subject1" style="width:100% ; size: 50px;padding:25px;background-color:#123456;color:white" /> 
-			<input type="button" name="Sub2" value="Subject2" style="width:100% ;size: 50px;padding:25px;background-color:#123456;color:white " />
-			<input type="button" name="Sub3" value="Subject3" style="width:100%;size: 50px;padding:25px;background-color: #123456;color:white" />
-			<input type="button" name="Sub4" value="Subject4" style="width:100% ;size: 50px;padding:25px;background-color: #123456;color:white" />
+		<div style="width:15%;margin-top:70px;float:left">
+			  <input class="subtn" type="button" name="Sub1" value="Subject1" style="width:100% ; size: 50px;padding:25px;background-color:#123456;color:white;border-radius:10px;margin-bottom:3px;" /> 
+			<input class="subtn" type="button" name="Sub2" value="Subject2" style="width:100% ;size: 50px;padding:25px;background-color:#123456;color:white;border-radius:10px;margin-bottom:3px ;" />
+			<input class="subtn" type="button" name="Sub3" value="Subject3" style="width:100%;size: 50px;padding:25px;background-color: #123456;color:white;border-radius:10px;margin-bottom:3px;" />
+			<input class="subtn" type="button" name="Sub4" value="Subject4" style="width:100% ;size: 50px;padding:25px;background-color: #123456;color:white;border-radius:10px;margin-bottom:3px;" />
 		
 		</div>
 	
 	<section >
 		<div style="width:60%;float:left;height: 470px;margin-top: 60px;margin-left: 70px;border :solid 5px #720245 " class="div-animate">
-			<h1 style="margin-left:100px;">QUIZ TiTLE:</h1>
-			<label><?php if(isset($SESSION['Title'])){ echo $_SESSION['Title']; }?></label>
-			<h2 style="margin-left:100px;">Quiz Sub TiTLE:</h2>
-			<label><?php if(isset($SESSION['Subtitle'])){ echo $_SESSION['Subtitle']; }?></label>
+			
+			<?php
+			include '../../includes/crtquiz.php';
+			?>
 			<input type="button" name="startqz" value="START QUIZ" onclick="document.location.href ='http://localhost/Ip/pages/student/quiz.php';" style="width:15% ;height:30%;background-color:#123456;color:white;margin-left: 380px;margin-top:200px;border-radius: 50%;font-size: 16px;border:0px;">
 			
 	</section>
 	</form>
-	<!-- #1CFBB1 -->
 </body>
 </html>
