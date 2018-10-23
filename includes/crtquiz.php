36bb7dec333a578094dea2b588d9cdff4f2bd391
Raj-Madheshia <rajsmadheshia@gmail.com>
2018-10-23 13:57:54 +0530

    Student subjects bug fixed

--- includes/crtquiz.php
+++ includes/crtquiz.php
@@ -19,9 +19,27 @@
 	}
 	
 	
+	$sql = "select * from $class where Subject= '$subject'";
+	$result = mysqli_query($conn,$sql);
+	if(mysqli_num_rows($result)>0 ){
+		while($row = mysqli_fetch_assoc($result)){
+			$a = $row['Tablename'];
+			$a1 = $row['Title'];
+			$a2 = $row['Subtitle'];
+			setcookie('examname',$a,time() +86400, '/');
+			setcookie('title',$a1,time() +86400, '/');
+			setcookie('subtitle',$a2,time() +86400, '/');
+			header('Location: ../pages/student/home.php');
+			break;
+		}
+	}else if(mysqli_num_rows($result) === 0 ) {
+		setcookie('examname'," ",time() +86400, '/');
+		setcookie('title'," ",time() +86400, '/');
+		setcookie('subtitle'," ",time() +86400, '/');
+		header('Location: ../pages/student/home.php');
+	}
 	
 	if(isset($_POST['startqz'])){
-
 		$sid = $_COOKIE['sid'];
 		$examname = $_COOKIE['examname'];
 		$qtablename = $sid."_".$examname;
@@ -35,5 +53,4 @@
 		setcookie('QuestionNo',$i,time() +86400, '/');
 		header("Location: ../pages/student/quiz.php");
 	}
-
 ?>
